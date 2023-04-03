47757cbab89256108e90de5ae08b57cc78708581
reqruter <reqruter@yandex.ru>
2023-04-03 20:09:44 +0300

    gitignore added

--- src/test/java/ru/netology/services/BestServiceTest.java
+++ src/test/java/ru/netology/services/BestServiceTest.java
@@ -9,16 +9,24 @@ class BestServiceTest {
     @Test
     public void calculateTest1() {
         BestService service = new BestService();
-        int actual = service.calculate (income:10000,expenses: 3000,threshold: 20000);
+        int income=10_000;
+        int expenses=3_000;
+        int threshold=20_000;
         int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void calculateTest2() {
         BestService service = new BestService();
-        int actual = service.calculate(income:100_000, expenses:60_000, threshold:150_000);
+        int income=100_000;
+        int expenses=60_000;
+        int threshold=150_000;
         int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
+
         Assertions.assertEquals(expected, actual);
     }
 }
